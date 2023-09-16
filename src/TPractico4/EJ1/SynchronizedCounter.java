201ea4f8d6aa59009020622752fcb9dd3e81c5aa
MorenoNicolas <nicomore4@gmail.com>
2023-09-16 19:31:20 -0300

    .

---
+++ src/TPractico4/EJ1/SynchronizedCounter.java
@@ -0,0 +1,15 @@
+package TPractico4.EJ1;
+
+public class SynchronizedCounter {
+    private int c = 0;
+
+    public synchronized void increment() {
+        c++;
+    }
+    public void decrement() {
+        c--;
+    }
+    public synchronized int value() {
+        return c;
+    }
+}
---
+++ src/TPractico4/EJ1/SynchronizedObjectCounter.java
@@ -0,0 +1,19 @@
+package TPractico4.EJ1;
+
+public class SynchronizedObjectCounter {
+    private int c = 0;
+
+    public void increment() {
+        synchronized (this) {
+            c++;
+        } // Este elemento debe ser casteado a Integer
+    }
+    public void decrement() {
+        synchronized (this) {
+            c--;
+        }
+    }
+    public int value() {
+        return c;
+    }
+}
