d98ab9395caa7fc1a3f45615d1a2dfb4fe943ef4
MorenoNicolas <nicomore4@gmail.com>
2023-09-24 00:47:22 -0300

    ejercicio 4 semaforos

--- src/TPractico4/EJ4/RecursoCompartido.java
+++ src/TPractico4/EJ4/RecursoCompartido.java
@@ -2,16 +2,31 @@ package TPractico4.EJ4;
 
 public class RecursoCompartido {
     
-    private Impresora impresora1;
-    private Impresora impresora2;
-    private Impresora impresora3;
+    private int cant = 3;
+    private Impresora[] impresora = new Impresora[cant];
+    
 
     public RecursoCompartido(){
-        impresora1 = new Impresora(1, true);
-        impresora2 = new Impresora(1, true);
-        impresora3 = new Impresora(1, true);
+        for(int i =0; i<cant ;i++){
+            impresora[i] = new Impresora(1, true);
+        }
     }
-    public void imprimir(){
-        
+    public void imprimir() throws InterruptedException{
+        int i =0;
+        boolean esperando = true;
+        while(esperando){
+            if(impresora[i].getDisponibilidad()==true){
+                //System.out.println(Thread.currentThread().getName());
+                impresora[i].noDisponible();
+                impresora[i].acquire();
+                System.out.println("La impresora: "+i+" esta imprimiendo...");
+                Thread.sleep(2000);
+                System.out.println("La impresora: "+i+" termino");
+                impresora[i].release();
+                esperando = false;
+                impresora[i].liberarDisponible();
+            }
+            i++;
+        }
     }
 }
